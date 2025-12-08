f82148c773f4a460d6153c478d4de5375551ec99
Thevakumar-Luheerathan <Luhee27@gmail.com>
2025-12-08 14:30:36 +0530

    Add workflow to publish installers

--- installers/windows/build.bat
+++ installers/windows/build.bat
@@ -26,9 +26,15 @@ if "%~4"=="" (
 
 
 @REM REM Extract ballerina.zip
-powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%~1', '.\temp_ballerina'); }"
-move ".\temp_ballerina\ballerina-*" ".\WixPackage\payload\Ballerina"
-if exist ".\temp_ballerina" rmdir /s /q ".\temp_ballerina"
+set "TEMP_BAL_DIR=C:\temp_ballerina_%RANDOM%_%TIME:~6,2%"
+if exist "%TEMP_BAL_DIR%" rmdir /s /q "%TEMP_BAL_DIR%"
+mkdir "%TEMP_BAL_DIR%"
+powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%~1', '%TEMP_BAL_DIR%'); }"
+mkdir ".\WixPackage\payload\Ballerina"
+REM Move the ballerina-* directory from temp to payload
+REM Move all contents from ballerina-xxxx to payload
+for /d %%D in ("%TEMP_BAL_DIR%\ballerina-*") do xcopy "%%D\*" ".\WixPackage\payload\Ballerina" /E /H /Y
+if exist "%TEMP_BAL_DIR%" rmdir /s /q "%TEMP_BAL_DIR%"
 if errorlevel 1 (
     echo Ballerina extraction failed
     exit /b 1
@@ -65,6 +71,18 @@ if errorlevel 1 (
     exit /b 1
 )
 
+REM Rename MSI output to include version
+set "MSI_ORIG=WixPackage\bin\x64\Release\en-US\WSO2-Integrator.msi"
+set "MSI_NEW=WixPackage\bin\x64\Release\en-US\wso2-integrator-%~4.msi"
+if exist "%MSI_ORIG%" (
+    ren "%MSI_ORIG%" "wso2-integrator-%~4.msi"
+    echo Renamed MSI to %MSI_NEW%
+) else (
+    echo MSI file not found: %MSI_ORIG%
+)
+
 REM Revert version placeholder in Package.wxs
 powershell -Command "(Get-Content '.\WixPackage\Package.wxs') -replace '%~4', '@VERSION@' | Set-Content '.\WixPackage\Package.wxs'"
+REM Remove payload directory after build
+if exist ".\WixPackage\payload" rmdir /s /q ".\WixPackage\payload"
 endlocal
