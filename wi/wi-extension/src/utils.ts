29caf6a26653e33940120d9ea31499e6dd82f7ed
kaje94 <a.kajendran@gmail.com>
2026-03-08 12:29:40 +0530

    add store and state manager

---
+++ wi/wi-extension/src/utils.ts
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import * as os from "os";
+import * as path from "path";
+
+export const getNormalizedPath = (filePath: string): string => {
+    if (os.platform() === "win32") {
+        return filePath.replace(/^\//, "").replace(/\//g, "\\");
+    }
+    return path.normalize(filePath);
+};
+
+export const isSamePath = (parent: string, sub: string): boolean => {
+    let normalizedParent = getNormalizedPath(parent).toLowerCase();
+    if (normalizedParent.endsWith("/")) {
+        normalizedParent = normalizedParent.slice(0, -1);
+    }
+
+    let normalizedSub = getNormalizedPath(sub).toLowerCase();
+    if (normalizedSub.endsWith("/")) {
+        normalizedSub = normalizedSub.slice(0, -1);
+    }
+
+    return normalizedParent === normalizedSub;
+};
+
+export const isSubpath = (parent: string, sub: string): boolean => {
+    let normalizedParent = getNormalizedPath(parent).toLowerCase();
+    if (normalizedParent.endsWith("/")) {
+        normalizedParent = normalizedParent.slice(0, -1);
+    }
+
+    let normalizedSub = getNormalizedPath(sub).toLowerCase();
+    if (normalizedSub.endsWith("/")) {
+        normalizedSub = normalizedSub.slice(0, -1);
+    }
+
+    if (normalizedParent === normalizedSub) {
+        return true;
+    }
+
+    const relative = path.relative(normalizedParent, normalizedSub);
+    return !!relative && !relative.startsWith("..") && !path.isAbsolute(relative);
+};
